e2b821023ce80459cae8896a15321189d1d12bb7
SCHEMACHANGEADMIN <pavanmishra678@yahoo.com>
2026-05-25 12:17:50 +0000

    rollback changes

--- rollback/V1.2__rollback_customer.sql
+++
@@ -1,2 +0,0 @@
--- Rollback for V1.2.1 - drop CUSTOMER table
-DROP TABLE IF EXISTS {{ env_schema }}.CUSTOMER;
--- rollback/V1.3__rollback_orders.sql
+++
@@ -1,2 +0,0 @@
--- Rollback for V1.3.1 - drop ORDERS table
-DROP TABLE IF EXISTS {{ env_schema }}.ORDERS;
